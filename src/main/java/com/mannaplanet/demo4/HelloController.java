60598334fafeca096da46b062547780de14b4329
이문환 <pyramidmine@gmail.com>
2022-05-16 14:27:43 +0900

    최초 커밋

---
+++ src/main/java/com/mannaplanet/demo4/HelloController.java
@@ -0,0 +1,15 @@
+package com.mannaplanet.demo4;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@Api(tags = {"Hello Controller"})
+@RestController
+public class HelloController {
+	@GetMapping("/api/hello")
+	public String hello() {
+		return "Hello!";
+	}
+}
